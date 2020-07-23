83bec4af93052e4cdd01533486b9ac3d8f6c3c8c
Vagner Nascimento <vagnernasc88@gmail.com>
2020-07-23 03:46:16 -0300

    tryng to adjust stress test

--- src/infra/data/amqp/AmqpPublisher.ts
+++ src/infra/data/amqp/AmqpPublisher.ts
@@ -15,12 +15,24 @@ class AmqpPublisher {
 
     private amqpConn: AmqConnection
     private amqpChannel: amqplib.Channel
+    /*
+        TODO: amqp conn error with 10k msgs:
+        2020-07-23 04:44:23.048Z - AmqConnection - error on try to connect into amqp server { Error: read ECONNRESET
+            at TCP.onStreamRead (internal/stream_base_commons.js:111:27)
+        cause:
+        { Error: read ECONNRESET
+            at TCP.onStreamRead (internal/stream_base_commons.js:111:27) errno: 'ECONNRESET', code: 'ECONNRESET', syscall: 'read' },
+        isOperational: true,
+        errno: 'ECONNRESET',
+        code: 'ECONNRESET',
+        syscall: 'read' }
 
+    */
     private async connect() {
         if(!this.amqpConn || !this.amqpConn.isConnected()) {
             this.amqpChannel = null
 
-            await this.amqpConn.connect()            
+            await this.amqpConn.connect()
         }
     }
 
@@ -34,16 +46,14 @@ class AmqpPublisher {
 
     public async publishMessage(queue: string, data: any) {
         try {
-            this.logInfo(`data to send to topic ${queue}:`, data)
-
             await this.connect()
-
+            
             if(!this.amqpChannel) this.amqpChannel = await this.amqpConn.getChannel()
+            
+            this.logInfo(`data to send to topic ${queue}:`, data)
 
             await this.amqpChannel.assertQueue(queue, { durable: false })
             await this.amqpChannel.sendToQueue(queue, Buffer.from(data))
-            
-            this.logInfo("data susccessfully published")
         } catch(err) {
             const msg = `error on try to publish data into ${queue}`
 
--- tests/stresstest.go
+++ tests/stresstest.go
@@ -1,79 +1,35 @@
 package main
-/*
-errors on node app:
 
-2020-07-20 07:04:40.827Z - error on to publish data into q-accounts  Error: No channels left to allocate
-    at Connection.C.freshChannel (/app/node_modules/amqplib/lib/connection.js:451:11)
-    at Channel.C.allocate (/app/node_modules/amqplib/lib/channel.js:53:29)
-    at tryCatcher (/app/node_modules/bluebird/js/release/util.js:16:23)
-    at Function.Promise.attempt.Promise.try (/app/node_modules/bluebird/js/release/method.js:39:29)
-    at Channel.C.open (/app/node_modules/amqplib/lib/channel_model.js:68:21)
-    at ChannelModel.CM.createChannel (/app/node_modules/amqplib/lib/channel_model.js:48:12)
-    at RabbitServer.<anonymous> (/app/dist/infra/data/amqp/RabbitServer.js:82:47)
-    at Generator.next (<anonymous>)
-    at /app/dist/infra/data/amqp/RabbitServer.js:8:71
-    at new Promise (<anonymous>)
-    at __awaiter (/app/dist/infra/data/amqp/RabbitServer.js:4:12)
-    at RabbitServer.newChannel (/app/dist/infra/data/amqp/RabbitServer.js:76:16)
-    at RabbitServer.<anonymous> (/app/dist/infra/data/amqp/RabbitServer.js:104:39)
-    at Generator.next (<anonymous>)
-    at /app/dist/infra/data/amqp/RabbitServer.js:8:71
-    at new Promise (<anonymous>)
-2020-07-20 07:04:40.827Z - error on to publish data into q-accounts  Error: No channels left to allocate
-    at Connection.C.freshChannel (/app/node_modules/amqplib/lib/connection.js:451:11)
-    at Channel.C.allocate (/app/node_modules/amqplib/lib/channel.js:53:29)
-    at tryCatcher (/app/node_modules/bluebird/js/release/util.js:16:23)
-    at Function.Promise.attempt.Promise.try (/app/node_modules/bluebird/js/release/method.js:39:29)
-    at Channel.C.open (/app/node_modules/amqplib/lib/channel_model.js:68:21)
-    at ChannelModel.CM.createChannel (/app/node_modules/amqplib/lib/channel_mevents.js:174
-      throw er; // Unhandled 'error' event
-      ^
-
-Error: write EPIPE
-    at WriteWrap.afterWrite [as oncomplete] (net.js:788:14)
-Emitted 'error' event at:
-    at Connection.emit (events.js:203:15)
-    at Connection.C.onSocketError (/app/node_modules/amqplib/lib/connection.js:353:10)
-    at Socket.emit (events.js:203:15)
-    at errorOrDestroy (internal/streams/destroy.js:107:12)
-    at onwriteError (_stream_writable.js:436:5)
-    at onwrite (_stream_writable.js:461:5)
-    at _destroy (internal/streams/destroy.js:49:7)
-    at Socket._destroy (net.js:613:3)
-    at Socket.destroy (internal/streams/destroy.js:37:8)
-    at WriteWrap.afterWrite [as oncomplete] (net.js:790:10)
-vagner@vnasc-note-sam:~$
-
-*/
 import (
 	"errors"
 	"fmt"
-	"github.com/streadway/amqp"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
-	"time"
-)
 
-func main() {
-	setTestParams()
-	runWithoutConsume()
-	//runWithConsume()
-}
+	"github.com/streadway/amqp"
+)
 
 var (
 	qtdSellers   int
 	qtdMerchants int
-	testTimeOut  time.Duration
 )
+
+/*
+	TODO: not all msgs are published. Error:
+	channel closed Exception (505) Reason: "UNEXPECTED_FRAME - expected content body, got non content body frame instead"
+*/
+func main() {
+	setTestParams()
+	publishMessages()
+}
 
 func setTestParams() {
 	// default params
-	qtdSellers = 800
-	qtdMerchants = 800
-	minutes := 10
+	qtdSellers = 200
+	qtdMerchants = 200
 
 	// env params
 	if qtd := os.Getenv("QTD_SELL"); len(qtd) > 0 {
@@ -82,56 +38,9 @@ func setTestParams() {
 	if qtd := os.Getenv("QTD_MERCH"); len(qtd) > 0 {
 		qtdMerchants, _ = strconv.Atoi(qtd)
 	}
-	if t := os.Getenv("MINUTES_TIMEOUT"); len(t) > 0 {
-		minutes, _ = strconv.Atoi(t)
-	}
-
-	testTimeOut = time.Minute * time.Duration(minutes)
 }
 
-func runWithConsume() {
-	if err, consumed := consumeAccounts(); err == nil {
-		start := time.Now()
-		errs := multiplexErrors(
-			pubMerchants(qtdMerchants),
-			pubSellers(qtdSellers),
-		)
-
-		for err := range errs {
-			if err != nil {
-				fmt.Println("pub err")
-				panic(err)
-			}
-		}
-
-		totalSent := qtdSellers + qtdMerchants
-		success := false
-		timeLimit := time.Now().Add(testTimeOut)
-
-		fmt.Println("waiting for accounts consume")
-
-		for time.Now().Before(timeLimit) {
-			if *consumed == totalSent {
-				success = true
-				break
-			}
-		}
-
-		if success {
-			fmt.Println(fmt.Sprintf("SUCCESS: STRESS TESTS COMPLETED IN %s", time.Since(start)))
-			fmt.Println(fmt.Sprintf("ALL %d MESSAGES PROCESSED", totalSent))
-		} else {
-			fmt.Println("TEST FAILED")
-			fmt.Println(fmt.Sprintf("PROCESSED %d OF %d MESSAGES", *consumed, totalSent))
-		}
-	} else {
-		fmt.Println("error on consume q-accounts", err)
-	}
-
-	os.Exit(0)
-}
-
-func runWithoutConsume() {
+func publishMessages() {
 	errs := multiplexErrors(
 		pubMerchants(qtdMerchants),
 		pubSellers(qtdSellers),
@@ -139,8 +48,7 @@ func runWithoutConsume() {
 
 	for err := range errs {
 		if err != nil {
-			fmt.Println("pub err")
-			panic(err)
+			fmt.Println("pub err", err)
 		}
 	}
 
@@ -150,19 +58,21 @@ func runWithoutConsume() {
 // AMQP
 type connection struct {
 	conn    *amqp.Connection
-	ch 	*amqp.Channel
+	ch      *amqp.Channel
 	connect sync.Once
 	isAlive bool
 }
 
 var singletonConn connection
 
-func getChannel() (*amqp.Channel, error) {
+func getChannel() *amqp.Channel {
 	var (
 		err error
 	)
 
-	singletonConn.connect.Do(func() {
+	if singletonConn.conn == nil || singletonConn.conn.IsClosed() {
+		singletonConn.ch = nil
+
 		var url string
 		if strings.ToUpper(os.Getenv("GO_ENV")) == "DOCKER" {
 			url = "amqp://guest:guest@js-rabbit-mq:5672"
@@ -171,17 +81,31 @@ func getChannel() (*amqp.Channel, error) {
 		}
 
 		singletonConn.conn, err = amqp.Dial(url)
-	})
+	}
 
-	if err == nil {
-		if singletonConn.conn == nil || singletonConn.conn.IsClosed() {
-			panic("rabbit connection is closed")
-		} else if singletonConn.ch == nil {
-			singletonConn.ch, err = singletonConn.conn.Channel()
+	if err != nil {
+		fmt.Println("error on connect into rabbit mq")
+		panic(err)
+	}
+
+	if singletonConn.ch == nil {
+		if singletonConn.ch, err = singletonConn.conn.Channel(); err == nil {
+			chErrs := singletonConn.ch.NotifyClose(make(chan *amqp.Error))
+
+			go func() {
+				for chErr := range chErrs {
+					fmt.Println("channel closed", chErr)
+
+					singletonConn.ch = nil
+				}
+			}()
+		} else {
+			fmt.Println("error on create a channel")
+			panic(err)
 		}
 	}
 
-	return singletonConn.ch, err
+	return singletonConn.ch
 }
 
 func publishMsg(data []byte, topic string) error {
@@ -191,28 +115,34 @@ func publishMsg(data []byte, topic string) error {
 		err error
 	)
 
-	if ch, err = getChannel(); err == nil {
-		qP, err = ch.QueueDeclare(
-			topic,
-			false,
-			false,
-			false,
-			false,
-			nil,
-		)
+	ch = getChannel()
 
-		if err == nil {
-			err = ch.Publish(
-				"",
-				qP.Name,
-				false,
-				false,
-				amqp.Publishing{
-					ContentType: "application/json",
-					Body:        data,
-				},
-			)
-		}
+	for ch == nil {
+		fmt.Println("channel null, trying to get other")
+
+		ch = getChannel()
+	}
+
+	qP, err = ch.QueueDeclare(
+		topic,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err == nil {
+		err = ch.Publish(
+			"",
+			qP.Name,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        data,
+			},
+		)
 	}
 
 	return err
@@ -229,15 +159,17 @@ func pubMerchants(qtd int) <-chan error {
 			if data, err := ioutil.ReadAll(json); err == nil {
 				for i := 1; i <= qtd; i++ {
 					fmt.Println(fmt.Sprintf("publishing merchant %d of %d", i, qtd))
-					if err := publishMsg(data, "q-merchants"); err != nil {
-						err = errors.New("q-merchants err: " + err.Error())
-						errs <- err
+					err := publishMsg(data, "q-merchants")
+					for err != nil {
+						errs <- errors.New("q-merchants err: " + err.Error())
+
+						err = publishMsg(data, "q-merchants")
 					}
 				}
 			}
+		} else {
+			errs <- err
 		}
-
-		errs <- err
 
 		close(errs)
 	}()
@@ -258,64 +190,22 @@ func pubSellers(qtd int) <-chan error {
 			if data, err := ioutil.ReadAll(json); err == nil {
 				for i := 1; i <= qtd; i++ {
 					fmt.Println(fmt.Sprintf("publishing seller %d of %d", i, qtd))
-					if err := publishMsg(data, "q-sellers"); err != nil {
-						err = errors.New("q-sellers err: " + err.Error())
-						errs <- err
+					err := publishMsg(data, "q-sellers")
+					for err != nil {
+						errs <- errors.New("q-sellers err: " + err.Error())
+
+						err = publishMsg(data, "q-sellers")
 					}
 				}
 			}
+		} else {
+			errs <- err
 		}
-
-		errs <- err
 
 		close(errs)
 	}()
 
 	return errs
-}
-
-func consumeAccounts() (error, *int) {
-	var (
-		ch        *amqp.Channel
-		processed int
-		err       error
-	)
-
-	if ch, err = getChannel(); err == nil {
-		var q amqp.Queue
-		q, err = ch.QueueDeclare(
-			"q-accounts",
-			false,
-			false,
-			false,
-			false,
-			nil,
-		)
-
-		if err == nil {
-			var msgs <-chan amqp.Delivery
-			msgs, err = ch.Consume(
-				q.Name,
-				"c-stress",
-				true,
-				false,
-				false,
-				false,
-				nil,
-			)
-
-			if err == nil {
-				go func() {
-					for _ = range msgs {
-						processed++
-						fmt.Println(fmt.Sprintf("processed %d messages", processed))
-					}
-				}()
-			}
-		}
-	}
-
-	return err, &processed
 }
 
 // Channel multiplexer
